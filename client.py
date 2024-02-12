f72dc5f51d5fbc616c28cf0239fef1aec705048d
tev <tevelshorz@gmail.com>
2024-02-12 13:41:58 +0800

    feat; added client and server connections to talk to each other
    feat; UI improvement

--- client.py
+++ client.py
@@ -8,8 +8,47 @@ import threading
 HOST = "127.0.0.1"
 PORT = 1234
 
+# Function to listen all messages sent from the server and print it out
+def listen_for_messages_from_server(client):
+    while True:
+        message = client.recv(2048).decode("utf-8")
+        if message != "":
+            username = message.split(":")[0]
+            content = message.split(":")[1]
+
+            print(f"[{username}]: {content}")
+            print("")
+        else:
+            print("Message received from client is empty")
+
+# Funtion to send the encoded message to the server
+def send_message_to_server(client):
+    while True:
+        message = input("Message: ")
+        if message != "":
+            client.sendall(message.encode())
+        else:
+            print("Empty message")
+            exit(0)
+
+# Function to prompt user for their username and send it over to the server
+def communicate_to_server(client):
+    username = input("Enter your username: ")
+    print("")
+    if username != "":
+        client.sendall(username.encode())
+    else:
+        print("Your username cannot be empty")
+        exit(0)
+
+    threading.Thread(target=listen_for_messages_from_server, args=(client, )).start()
+
+    send_message_to_server(client)
+
 def main():
-    # Creating a socket 
+    # Creating a socket
+    # AF_INET: Using IPv4 addresses
+    # SOCK_STEAM: Using TCP packets for communication  
     client = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 
     # Connect to the server
@@ -18,6 +57,11 @@ def main():
         print(f"Successfully connected to server {HOST} on port {PORT}!")
     except:
         print(f"ERROR! Unable connect to host: {HOST} on port: {PORT}")
+
+    communicate_to_server(client)
+
+
+
 
 if __name__ == '__main__':
     main()
--- server.py
+++ server.py
@@ -9,6 +9,43 @@ import threading
 HOST = "127.0.0.1" # Local host
 PORT = 1234 # Any port from 0-65535
 LISTENER_LIMIT = 3
+active_clients = [] # List of all currently connected users
+
+# Function to listen for any incoming messages from connected clients
+def listen_for_messages(client, username):
+    while True:
+        message = client.recv(2048).decode("utf-8")
+        if message != "":
+            final_msg = username + ":" + message
+            send_messages_to_all(final_msg) 
+        else:
+            print(f"Message sent from {username} is empty.")
+
+# Function to send message to a single connected client
+def send_message_to_client(client, message):
+    client.sendall(message.encode()) # Encode the message for encryption
+
+# Function to send any new message to all the clients that are connected to the server
+def send_messages_to_all(message):
+    for user in active_clients:
+        send_message_to_client(user[1], message)
+
+# Function to handle client
+def client_handler(client):
+    # Server listening for client message containing username
+    while True:
+        username = client.recv(2048).decode("utf-8")
+        if username != "":
+            active_clients.append((username, client))
+            prompt_message = "SERVER:" + f"{username}" + "added to the chat" 
+            send_messages_to_all(prompt_message)
+            break;
+        else:
+            print("Client does not have a username!")
+
+    threading.Thread(target=listen_for_messages(client, username, )).start()
+
+
 
 # Main function
 def main():
@@ -32,7 +69,8 @@ def main():
         client, address = server.accept()
         print(f"Successfully connected to client: {address[0]} {address[1]}")
 
-
+        # Every time a client connects, the thread will start
+        threading.Thread(target=client_handler, args=(client, )).start()
 
 # Only call the main function when server.py is runned
 if __name__ == '__main__':
