4e7e606d4c2b75e1f343734c58e03b763ae909de
tev <tevelshorz@gmail.com>
2024-02-12 11:47:19 +0800

    feat; added code for client.py and server.py to connect to each other

--- client.py
+++ client.py
@@ -1,1 +1,29 @@
+# Sends messages to the server
+# Receiver messages from the server
+
+# Required modules
+import socket
+import threading
+
+HOST = "127.0.0.1"
+PORT = 1234
+
+def main():
+    # Creating a socket 
+    client = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+
+    # Connect to the server
+    try:
+        client.connect((HOST, PORT))
+        print(f"Successfully connected to server {HOST} on port {PORT}!")
+    except:
+        print(f"ERROR! Unable connect to host: {HOST} on port: {PORT}")
+
+if __name__ == '__main__':
+    main()
+
+
+
+
+
 
--- server.py
+++ server.py
@@ -1,1 +1,39 @@
+# Keep listening for any new client connections or messages
+# Receive messages from client
+# Send messages to other clients
 
+# Required modules
+import socket
+import threading
+
+HOST = "127.0.0.1" # Local host
+PORT = 1234 # Any port from 0-65535
+LISTENER_LIMIT = 3
+
+# Main function
+def main():
+    # Creating the server socket
+    # AF_INET: Using IPv4 addresses
+    # SOCK_STEAM: Using TCP packets for communication 
+    server = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+
+    try:
+        # Bind to that specific host and port
+        server.bind((HOST, PORT))
+        print(f"Running the server {HOST} on {PORT} ")
+    except:
+        print(f"ERROR! Unable to bind to host: {HOST} on port: {PORT}")
+
+    # Set server limit of the amount of clients that can connect to the server
+    server.listen(LISTENER_LIMIT)
+
+    # Keep listening to client connections
+    while True:
+        client, address = server.accept()
+        print(f"Successfully connected to client: {address[0]} {address[1]}")
+
+
+
+# Only call the main function when server.py is runned
+if __name__ == '__main__':
+    main()
